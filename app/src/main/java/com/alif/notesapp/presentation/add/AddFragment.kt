53e885366f758721da9ebe5a5289af327217e29c
SyauqiAlifI <vstudio776@gmail.com>
2022-03-03 14:40:26 +0700

    pushiiing#2

--- app/src/main/java/com/alif/notesapp/presentation/add/AddFragment.kt
+++ app/src/main/java/com/alif/notesapp/presentation/add/AddFragment.kt
@@ -1,22 +1,56 @@
 package com.alif.notesapp.presentation.add
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.alif.notesapp.MainActivity
 import com.alif.notesapp.R
+import com.alif.notesapp.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding as FragmentAddBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        _binding = FragmentAddBinding.inflate(layoutInflater)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setHasOptionsMenu(true)
+
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarAdd.apply {
+            setupWithNavController(navController, appBarConfiguration)
+            (requireActivity() as MainActivity).setSupportActionBar(this)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_save, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_save -> {
+                findNavController().navigate(R.id.action_addFragment_to_homeFragment)
+                Toast.makeText(context, "Successfully Add Note..", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
