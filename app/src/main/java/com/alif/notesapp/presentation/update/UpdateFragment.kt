ab1b0d90034407418c8ff2485970eeacf150426a
SyauqiAlifI <vstudio776@gmail.com>
2022-03-07 14:35:49 +0700

    7 Mar 2022 #2

--- app/src/main/java/com/alif/notesapp/presentation/update/UpdateFragment.kt
+++ app/src/main/java/com/alif/notesapp/presentation/update/UpdateFragment.kt
@@ -1,22 +1,64 @@
 package com.alif.notesapp.presentation.update
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.alif.notesapp.MainActivity
 import com.alif.notesapp.R
+import com.alif.notesapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding as FragmentUpdateBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        _binding = FragmentUpdateBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        setHasOptionsMenu(true)
+
+        val navController = findNavController()
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        binding.toolbarUpdate.apply {
+            setupWithNavController(navController, appBarConfiguration)
+            (requireActivity() as MainActivity).setSupportActionBar(this)
+            navController.addOnDestinationChangedListener {_, destination, _ ->
+                when (destination.id) {
+                    R.id.updateFragment -> this.setNavigationIcon(R.drawable.ic_left_arrow)
+                }
+            }
+        }
+
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_save, menu)
+        val item = menu.findItem(R.id.menu_save)
+        item.actionView.findViewById<AppCompatImageButton>(R.id.btn_save).setOnClickListener {
+            findNavController().navigate(R.id.action_updateFragment_to_detailFragment2)
+            Toast.makeText(context, "Note Has Been Updated.", Toast.LENGTH_SHORT).show()
+        }
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
